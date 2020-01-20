41f4662a1d3b2bb2ba9fb6e9a3ebab4b432da189
Cole Williamson <williamsoncole@gmail.com>
2020-01-19 22:02:59 -0600

    Added main app

--- web_project/web_project/urls.py
+++ web_project/web_project/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main_pages.views import home_view
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view, name='home'),
+
 ]
+
